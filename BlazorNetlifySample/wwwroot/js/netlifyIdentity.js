bbf497efabd96a79db289bf1518abfb18fc209b6
nobu17 <lunacy171710@yahoo.co.jp>
2020-12-09 23:11:33 +0900

    test netlify

---
+++ BlazorNetlifySample/wwwroot/js/netlifyIdentity.js
@@ -0,0 +1,26 @@
+﻿let dotNetObjRef;
+export function init(dotNetObj) {
+    if (!dotNetObj) {
+        throw err("dotNetObj should be not null");
+    }
+    dotNetObjRef = dotNetObj;
+    netlifyIdentity.on('login', user => dotNetObjRef.invokeMethod("OnLogin", user));
+    netlifyIdentity.on('logout', () => dotNetObjRef.invokeMethod("OnLoggedOut"));
+    netlifyIdentity.on('error', err => dotNetObjRef.invokeMethod("OnError", err));
+}
+
+export function getCurrentUser() {
+    return netlifyIdentity.currentUser();
+}
+
+export function openDialog() {
+    netlifyIdentity.open();
+}
+
+export function closeDialog() {
+    netlifyIdentity.close();
+}
+
+export function logout() {
+    netlifyIdentity.logout();
+}
